c345a3d93cecf2e75ca52480e1e37fd836ddafce
ISR-STL <israelaguiar@yahoo.com>
2025-08-24 16:19:58 -0300

    chore: enforce LF on YAML/Shell

--- run-backend-8813.ps1
+++ run-backend-8813.ps1
@@ -2,4 +2,6 @@
 $env:PYTHONPATH='C:\AgenteManus'
 Start-Process -FilePath 'C:\AgenteManus\venv\Scripts\python.exe' -WorkingDirectory 'C:\AgenteManus' -ArgumentList @(
   '-m','uvicorn','compat.server:app','--host','127.0.0.1','--port','8813','--log-level','info'
-) -WindowStyle Minimized
+) -WindowStyle Hidden
+
+
